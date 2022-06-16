5b1a6e588facc4a2d0a2b56cadb47fc09301bc1b
Carlos Enrique Sánchez Zavala <86889131+Charlyesz@users.noreply.github.com>
2022-06-16 00:31:22 -0500

    Add files via upload

--- maquina_turing.php
+++ maquina_turing.php
@@ -1,130 +1,130 @@
 <?php
-	$Guardado=Array();
+	$Guardado=Array(); // Crea el arreglo
 ?>
 <html>
-<title>Practica No. 7</title>
+<title>Practica Final</title>
 <body>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<h1><center> Practica No. 7 <br /></center></h1>
+	<h1><center> Practica Final <br /></center></h1>
 	<center> Maquina Turing </center>
 	<center>Manual (Max 20) o aleatorio</center>
 <form method="post" align="center">
 <h4>Ingresa tu cadena iniciando con 0 ó escribe aleatorio<input class="boton" type="search" name="total" MAXLENGTH=20 size="25"><a><input type="submit" class="boton" value="Generar"></a></h4>
 </form>
 <?php
-	$cadena=$_POST['total'];
-		function generate_string($length) 
+	$cadena=$_POST['total']; //Caja de texto para ingresar la cadena de texto
+		function generate_string($length) // Es la funcion la cual crea la cadena de caracteres de ceros y unos
 		{
-		    $characters = '01';
-		    $charactersLength = strlen($characters);
-		    $randomString = '';
-		    for ($i = 0; $i < $length; $i++) {
-		        $randomString .= $characters[rand(0, $charactersLength - 1)];
-		    }
-		    return $randomString;
+		  $characters = '01'; //Caracteres de la cadena
+		  $charactersLength = strlen($characters); //Tamaño de la cadena
+		  $randomString = '';
+		  for ($i = 0; $i < $length; $i++) {
+		       $randomString .= $characters[rand(0, $charactersLength - 1)]; // Crea la cadena aleatorio de los caracteres
+		   }
+		  return $randomString;
 		} 
-	if($cadena<>"aleatorio")
+	if($cadena<>"aleatorio") // Analiza el texto insgresado en el campo de texto
 	{
 		$tot=$cadena;
 		echo "<center>Maquina de turing manual</center>";
-		$c=$d=$b=$a=$i=0;//posicion izquierda a derecha
+		$c=$d=$b=$a=$i=0;// I posicion izquierda a derecha
 		$tamaño = strlen($cadena)-1;
-		$j=$tamaño;//posicion derecha a izquiersa
-		$manual=fopen("manual_maquina_turing.txt","w");
+		$j=$tamaño;// J posicion derecha a izquiersa
+		$manual=fopen("manual_maquina_turing.txt","w"); // Crea el archivo txt o lo sobreescribe el archivo
 		echo "<center>".$tot."</center>";
-		$total=$tamaño*2;
+		$total=$tamaño*2; // Control de ciclo
 		while($c<=$total)
 		{
-			$Guardado[$b]=$tot;
-			$b++;
-			if($tot[$i]=='0')
+			$Guardado[$b]=$tot; // Guarda en el arreglo
+			$b++; // Posicion del arreglo
+			if($tot[$i]=='0') 
 			{
-				$tot[$i]="X";
+				$tot[$i]="X"; // Reemplaza X
 				echo "<center>0->q0->X</center>";
-				$Guardado[$b]="0[".$i."]->q0->X";
-				$b++;
-				$Guardado[$b]=$tot;
-				$i++;
-				$b++;
-				$c++;
+				$Guardado[$b]="0[".$i."]->q0->X"; // Guarda en el arreglo
+				$b++; // Posicion del Arreglo 
+				$Guardado[$b]=$tot; // Guarda en el arreglo
+				$i++; // Movimiento de posicion de izqueirda a derecha
+				$b++; // Posicion del arreglo
+				$c++; // Control
 				echo "<center>".$tot."</center>";
 			}
 			elseif($tot[$j]=='1')
 			{
-					$tot[$j]="Y";				
+					$tot[$j]="Y";	// Reemplaza Y
 					echo "<center>1->q1->Y</center>";
-					$Guardado[$b]="1[".$j."]->q1->Y";
-					$b++;
-					$Guardado[$b]=$tot;
-					$j--;
-					$b++;
-					$c++;
+					$Guardado[$b]="1[".$j."]->q1->Y"; // Guarda en el arreglo
+					$b++; // Posicion del arreglo
+					$Guardado[$b]=$tot; // Guarda en el arreglo
+					$j--; // Posicion de derecha a izquierda
+					$b++; // Posicion del arreglo
+					$c++; // Control
 					echo "<center>".$tot."</center>";
 			}
 			else
 			{
-				$j--;
-				$i++;
-				$c++;
+				$j--; // Movimiento de derecha a izquierda
+				$i++; // Movimiento de izquierda a derecha
+				$c++; // Control
 			}
 		}
 		echo "<center>La cadena fue reemplazada correctamente.</center>";
-		foreach ($Guardado as $key => $value) 
+		foreach ($Guardado as $key => $value) // Muestra y guarda en el archivo txt
 		{
-			fwrite($manual,"[".$key."] ".$value.PHP_EOL);
+			fwrite($manual,"[".$key."] ".$value.PHP_EOL); // Escribe en el archivo txt
 		}
-		fclose($manual);
+		fclose($manual); // Cierra el archivo txt
 	}
 	else
 	{
-		$numero=rand(1,10000); //j es la posicion izquierda a derecha
+		$numero=rand(21,10000); 
 		$tot=generate_string($numero);
 		$aleatorio=fopen("aleatorio_maquina_turing.txt","w");
 		echo "<center>Maquina de turing aleatoria<br>".$tot."<br></center>";
-		$c=$d=$b=$a=$i=0;//posicion izquierda a derecha
-		$j=$numero;//posicion derecha a izquiersa
-		$total=$numero*2;
+		$c=$d=$b=$a=$i=0;// I posicion izquierda a derecha
+		$j=$numero;// J posicion derecha a izquiersa
+		$total=$numero*2; // Control
 		while($c<=$total)
 		{
-			$Guardado[$b]=$tot;
-			$b++;
+			$Guardado[$b]=$tot; // Guarda en el arreglo
+			$b++; // Posicion del arreglo
 			if($tot[$i]=='0')
 			{
 				$tot[$i]="X";
 				echo "<center>0->q0->X</center>";
-				$Guardado[$b]="0[".$i."]->q0->X";
-				$b++;
-				$Guardado[$b]=$tot;
-				$i++;
-				$b++;
-				$c++;
+				$Guardado[$b]="0[".$i."]->q0->X"; // Guarda en el arreglo
+				$b++; // Posicion del arreglo
+				$Guardado[$b]=$tot; // Guarda en el arreglo
+				$i++; // Posicion de derecha a izquierda
+				$b++; // Posicion del arreglo
+				$c++; // Control
 				echo "<center>".$tot."</center>";
 			}
 			elseif($tot[$j]=='1')
 			{
 					$tot[$j]="Y";				
 					echo "<center>1->q1->Y</center>";
-					$Guardado[$b]="1[".$j."]->q1->Y";
-					$b++;
-					$Guardado[$b]=$tot;
-					$j--;
-					$b++;
-					$c++;
+					$Guardado[$b]="1[".$j."]->q1->Y"; // Guarda en el arreglo
+					$b++; // Posicion del arreglo
+					$Guardado[$b]=$tot; // Guarda en el arreglo
+					$j--; // Posicion de derecha a izquierda
+					$b++; // Posicion del arreglo
+					$c++; // Control
 					echo "<center>".$tot."</center>";
 			}
 			else
 			{
-				$j--;
-				$i++;
-				$c++;
+				$j--; // Movimiento de derecha a izquierda
+				$i++; // Moviemiento de izquierda a derecha
+				$c++; // Control
 			}
 		}
 		echo "<center>La cadena fue reemplazada correctamente.</center>";
-		foreach ($Guardado as $key => $value) 
+		foreach ($Guardado as $key => $value) // Muestra y guarad todo el contenido del arreglo
 		{
-			fwrite($aleatorio,"[".$key."]=".$value.PHP_EOL);
+			fwrite($aleatorio,"[".$key."]=".$value.PHP_EOL); // Escribe en el archivo txt
 		}
-		fclose($aleatorio);
+		fclose($aleatorio); // Cierra el archiva txt
 	}
 ?>
 </body>
